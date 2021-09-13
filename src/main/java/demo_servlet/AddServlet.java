6afb2dbae5b5c76ec81d099372af40c2e52fc2c1
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-13 14:39:12 +0600

    HttpSession

--- src/main/java/demo_servlet/AddServlet.java
+++ src/main/java/demo_servlet/AddServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /*import org.apache.el.parser.AstInteger;
 import org.eclipse.jdt.internal.compiler.codegen.IntegerCache;*/
@@ -31,6 +32,10 @@ public class AddServlet extends HttpServlet{
 		 * req.setAttribute("sum", sum);
 		 */
 		
-		res.sendRedirect("sqr?sum="+sum);
+		/*
+		 * HttpSession session = req.getSession(); session.setAttribute("sum", sum);
+		 */
+		res.sendRedirect("sqr");
+		
 	}
 }
--- src/main/java/demo_servlet/SqrServlet.java
+++ src/main/java/demo_servlet/SqrServlet.java
@@ -6,11 +6,18 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqrServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
-		int s = Integer.parseInt(req.getParameter("sum"));
+		/* int s = Integer.parseInt(req.getParameter("sum")); */
+		
+		/*
+		 * HttpSession session = req.getSession(); int s =
+		 * (int)session.getAttribute("sum");
+		 */
+		
 		s = s*s;
 		 
 		PrintWriter out = res.getWriter();
